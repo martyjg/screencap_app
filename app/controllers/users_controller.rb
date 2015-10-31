19c5a186fbbff53b4e2ac32eeed2d1e79872e74d
martyjg <martinjosephgormley@gmail.com>
2015-10-31 18:21:58 +0000

    generated user controller for index and show which are not included with the generation of devise registration controllers.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
+
+
+
+
+  def index
+    @users = User.all
+  end
+​
+  def show
+    @user = User.find(params[:id])
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   resources :screenshots
   devise_for :users
   root to: "screenshots#index"
